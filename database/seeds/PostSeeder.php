6aba5dd6e77952858615067354101cb835f2ac96
niculas1989 <nicolas.maranzano@libero.it>
2022-03-25 17:26:12 +0100

    added connection with database and Posts - all wor

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Post;
+use Faker\Generator as Faker;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->title = $faker->text(20);
+            $post->content = $faker->paragraphs(2, true);
+            $post->image = $faker->imageUrl(300, 300);
+            $post->slug = Str::slug($post->title, '-');
+            $post->save();
+        }
+    }
+}
